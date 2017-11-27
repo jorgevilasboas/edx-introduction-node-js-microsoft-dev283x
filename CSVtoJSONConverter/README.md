174f1b05451a725bbf87454c82656f4b11392305
jorgevilasboas <jorge.sergipe@gmail.com>
2017-11-27 16:08:51 -0300

    Adds Module 1 Assignments and Node core http

---
+++ CSVtoJSONConverter/README.md
@@ -0,0 +1,15 @@
+1) Clone the repository
+
+git clone https://github.com/jorgevilasboas/edx-node-intro-assignment1.git
+
+2) Run
+
+node app.js
+
+3) Compare the json files
+
+node compare.js customer-data.json solution.json
+
+
+---------------------------------------------------------------------------------------------
+
--- Node Core/008 - HTTP Client with Core http/http-get-no-buff.js
+++ Node Core/008 - HTTP Client with Core http/http-get-no-buff.js
@@ -1,11 +1,13 @@
-const http = require('http')
-const url = 'http://nodeprogram.com'
+const http = require('http');
+const url = 'http://nodeprogram.com';
 http.get(url, (response) => {
+    let c = 0;
     response.on('data', (chunk) => {
+        c++;
         console.log(chunk.toString('utf8'))
     })
     response.on('end', () => {
-        console.log('response has ended')
+        console.log(`response has ended with ${c} chunk(s)`);
     })
 }).on('error', (error) => {
     console.error(`Got error: ${error.message}`)
